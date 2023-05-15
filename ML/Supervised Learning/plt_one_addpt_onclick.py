395755a6c867fd6b90f9dc0e5a95c8a9e9b7afc3
GCastillo <gcastillo@ucdavis.edu>
2023-05-15 17:37:16 -0600

    Added labs on classification

--- ML/Supervised Learning/plt_one_addpt_onclick.py
+++ ML/Supervised Learning/plt_one_addpt_onclick.py
@@ -3,30 +3,35 @@ import copy
 from ipywidgets import Output
 from matplotlib.widgets import Button, CheckButtons
 from matplotlib.patches import FancyArrowPatch
-from lab_utils_common import np, plt, dlblue, dlorange, sigmoid, dldarkred, gradient_descent
+from lab_utils_common import np, plt, dlblue, dlorange, dldarkred
+from lab_utils_logistic import sigmoid, gradient_descent
 
 # for debug
-#output = Output() # sends hidden error messages to display when using widgets
-#display(output)
+# output = Output() # sends hidden error messages to display when using widgets
+# display(output)
+
 
 class plt_one_addpt_onclick:
     """ class to run one interactive plot """
+
     def __init__(self, x, y, w, b, logistic=True):
-        self.logistic=logistic
+        self.logistic = logistic
         pos = y == 1
         neg = y == 0
 
-        fig,ax = plt.subplots(1,1,figsize=(8,4))
+        fig, ax = plt.subplots(1, 1, figsize=(8, 4))
         fig.canvas.toolbar_visible = False
         fig.canvas.header_visible = False
         fig.canvas.footer_visible = False
 
         plt.subplots_adjust(bottom=0.25)
-        ax.scatter(x[pos], y[pos], marker='x', s=80, c = 'red', label="malignant")
-        ax.scatter(x[neg], y[neg], marker='o', s=100, label="benign", facecolors='none', edgecolors=dlblue,lw=3)
-        ax.set_ylim(-0.05,1.1)
+        ax.scatter(x[pos], y[pos], marker='x',
+                   s=80, c='red', label="malignant")
+        ax.scatter(x[neg], y[neg], marker='o', s=100, label="benign",
+                   facecolors='none', edgecolors=dlblue, lw=3)
+        ax.set_ylim(-0.05, 1.1)
         xlim = ax.get_xlim()
-        ax.set_xlim(xlim[0],xlim[1]*2)
+        ax.set_xlim(xlim[0], xlim[1]*2)
         ax.set_ylabel('y')
         ax.set_xlabel('Tumor Size')
         self.alegend = ax.legend(loc='lower right')
@@ -35,33 +40,37 @@ class plt_one_addpt_onclick:
         else:
             ax.set_title("Example of Linear Regression on Categorical Data")
 
-        ax.text(0.65,0.8,"[Click to add data points]", size=10, transform=ax.transAxes)
+        ax.text(0.65, 0.8, "[Click to add data points]",
+                size=10, transform=ax.transAxes)
 
-        axcalc   = plt.axes([0.1, 0.05, 0.38, 0.075])  #l,b,w,h
-        axthresh = plt.axes([0.5, 0.05, 0.38, 0.075])  #l,b,w,h
+        axcalc = plt.axes([0.1, 0.05, 0.38, 0.075])  # l,b,w,h
+        axthresh = plt.axes([0.5, 0.05, 0.38, 0.075])  # l,b,w,h
         self.tlist = []
 
         self.fig = fig
-        self.ax = [ax,axcalc,axthresh]
+        self.ax = [ax, axcalc, axthresh]
         self.x = x
         self.y = y
         self.w = copy.deepcopy(w)
         self.b = b
-        f_wb = np.matmul(self.x.reshape(-1,1), self.w) + self.b
+        f_wb = np.matmul(self.x.reshape(-1, 1), self.w) + self.b
         if self.logistic:
             self.aline = self.ax[0].plot(self.x, sigmoid(f_wb), color=dlblue)
-            self.bline = self.ax[0].plot(self.x, f_wb, color=dlorange,lw=1)
+            self.bline = self.ax[0].plot(self.x, f_wb, color=dlorange, lw=1)
         else:
             self.aline = self.ax[0].plot(self.x, sigmoid(f_wb), color=dlblue)
 
         self.cid = fig.canvas.mpl_connect('button_press_event', self.add_data)
         if self.logistic:
-            self.bcalc = Button(axcalc, 'Run Logistic Regression (click)', color=dlblue)
+            self.bcalc = Button(
+                axcalc, 'Run Logistic Regression (click)', color=dlblue)
             self.bcalc.on_clicked(self.calc_logistic)
         else:
-            self.bcalc = Button(axcalc, 'Run Linear Regression (click)', color=dlblue)
+            self.bcalc = Button(
+                axcalc, 'Run Linear Regression (click)', color=dlblue)
             self.bcalc.on_clicked(self.calc_linear)
-        self.bthresh = CheckButtons(axthresh, ('Toggle 0.5 threshold (after regression)',))
+        self.bthresh = CheckButtons(
+            axthresh, ('Toggle 0.5 threshold (after regression)',))
         self.bthresh.on_clicked(self.thresh)
         self.resize_sq(self.bthresh)
 
@@ -73,25 +82,27 @@ class plt_one_addpt_onclick:
             y_coord = event.ydata
 
             if y_coord > 0.5:
-                self.ax[0].scatter(x_coord, 1, marker='x', s=80, c = 'red' )
-                self.y = np.append(self.y,1)
+                self.ax[0].scatter(x_coord, 1, marker='x', s=80, c='red')
+                self.y = np.append(self.y, 1)
             else:
-                self.ax[0].scatter(x_coord, 0, marker='o', s=100, facecolors='none', edgecolors=dlblue,lw=3)
-                self.y = np.append(self.y,0)
-            self.x = np.append(self.x,x_coord)
+                self.ax[0].scatter(x_coord, 0, marker='o', s=100,
+                                   facecolors='none', edgecolors=dlblue, lw=3)
+                self.y = np.append(self.y, 0)
+            self.x = np.append(self.x, x_coord)
         self.fig.canvas.draw()
 
 #   @output.capture()  # debug
     def calc_linear(self, event):
         if self.bthresh.get_status()[0]:
             self.remove_thresh()
-        for it in [1,1,1,1,1,2,4,8,16,32,64,128,256]:
-            self.w, self.b, _ = gradient_descent(self.x.reshape(-1,1), self.y.reshape(-1,1),
-                                                 self.w.reshape(-1,1), self.b, 0.01, it,
+        for it in [1, 1, 1, 1, 1, 2, 4, 8, 16, 32, 64, 128, 256]:
+            self.w, self.b, _ = gradient_descent(self.x.reshape(-1, 1), self.y.reshape(-1, 1),
+                                                 self.w.reshape(-1,
+                                                                1), self.b, 0.01, it,
                                                  logistic=False, lambda_=0, verbose=False)
             self.aline[0].remove()
             self.alegend.remove()
-            y_hat = np.matmul(self.x.reshape(-1,1), self.w) + self.b
+            y_hat = np.matmul(self.x.reshape(-1, 1), self.w) + self.b
             self.aline = self.ax[0].plot(self.x, y_hat, color=dlblue,
                                          label=f"y = {np.squeeze(self.w):0.2f}x+({self.b:0.2f})")
             self.alegend = self.ax[0].legend(loc='lower right')
@@ -104,19 +115,20 @@ class plt_one_addpt_onclick:
     def calc_logistic(self, event):
         if self.bthresh.get_status()[0]:
             self.remove_thresh()
-        for it in [1, 8,16,32,64,128,256,512,1024,2048,4096]:
-            self.w, self.b, _ = gradient_descent(self.x.reshape(-1,1), self.y.reshape(-1,1),
-                                                 self.w.reshape(-1,1), self.b, 0.1, it,
+        for it in [1, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]:
+            self.w, self.b, _ = gradient_descent(self.x.reshape(-1, 1), self.y.reshape(-1, 1),
+                                                 self.w.reshape(-1,
+                                                                1), self.b, 0.1, it,
                                                  logistic=True, lambda_=0, verbose=False)
             self.aline[0].remove()
             self.bline[0].remove()
             self.alegend.remove()
-            xlim  = self.ax[0].get_xlim()
+            xlim = self.ax[0].get_xlim()
             x_hat = np.linspace(*xlim, 30)
-            y_hat = sigmoid(np.matmul(x_hat.reshape(-1,1), self.w) + self.b)
+            y_hat = sigmoid(np.matmul(x_hat.reshape(-1, 1), self.w) + self.b)
             self.aline = self.ax[0].plot(x_hat, y_hat, color=dlblue,
                                          label="y = sigmoid(z)")
-            f_wb = np.matmul(x_hat.reshape(-1,1), self.w) + self.b
+            f_wb = np.matmul(x_hat.reshape(-1, 1), self.w) + self.b
             self.bline = self.ax[0].plot(x_hat, f_wb, color=dlorange, lw=1,
                                          label=f"z = {np.squeeze(self.w):0.2f}x+({self.b:0.2f})")
             self.alegend = self.ax[0].legend(loc='lower right')
@@ -125,7 +137,6 @@ class plt_one_addpt_onclick:
         if self.bthresh.get_status()[0]:
             self.draw_thresh()
             self.fig.canvas.draw()
-
 
     def thresh(self, event):
         if self.bthresh.get_status()[0]:
@@ -140,24 +151,26 @@ class plt_one_addpt_onclick:
         xp5 = -self.b/ws if self.logistic else (0.5 - self.b) / ws
         ylim = self.ax[0].get_ylim()
         xlim = self.ax[0].get_xlim()
-        a = self.ax[0].fill_between([xlim[0], xp5], [ylim[1], ylim[1]], alpha=0.2, color=dlblue)
-        b = self.ax[0].fill_between([xp5, xlim[1]], [ylim[1], ylim[1]], alpha=0.2, color=dldarkred)
-        c = self.ax[0].annotate("Malignant", xy= [xp5,0.5], xycoords='data',
-             xytext=[30,5],textcoords='offset points')
+        a = self.ax[0].fill_between(
+            [xlim[0], xp5], [ylim[1], ylim[1]], alpha=0.2, color=dlblue)
+        b = self.ax[0].fill_between(
+            [xp5, xlim[1]], [ylim[1], ylim[1]], alpha=0.2, color=dldarkred)
+        c = self.ax[0].annotate("Malignant", xy=[xp5, 0.5], xycoords='data',
+                                xytext=[30, 5], textcoords='offset points')
         d = FancyArrowPatch(
             posA=(xp5, 0.5), posB=(xp5+1.5, 0.5), color=dldarkred,
             arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
         )
         self.ax[0].add_artist(d)
 
-        e = self.ax[0].annotate("Benign", xy= [xp5,0.5], xycoords='data',
-                     xytext=[-70,5],textcoords='offset points', ha='left')
+        e = self.ax[0].annotate("Benign", xy=[xp5, 0.5], xycoords='data',
+                                xytext=[-70, 5], textcoords='offset points', ha='left')
         f = FancyArrowPatch(
             posA=(xp5, 0.5), posB=(xp5-1.5, 0.5), color=dlblue,
             arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
         )
         self.ax[0].add_artist(f)
-        self.tlist = [a,b,c,d,e,f]
+        self.tlist = [a, b, c, d, e, f]
 
         self.fig.canvas.draw()
 
@@ -169,12 +182,12 @@ class plt_one_addpt_onclick:
 
     def resize_sq(self, bcid):
         """ resizes the check box """
-        #future reference
+        # future reference
         #print(f"width  : {bcid.rectangles[0].get_width()}")
         #print(f"height : {bcid.rectangles[0].get_height()}")
         #print(f"xy     : {bcid.rectangles[0].get_xy()}")
         #print(f"bb     : {bcid.rectangles[0].get_bbox()}")
-        #print(f"points : {bcid.rectangles[0].get_bbox().get_points()}")  #[[xmin,ymin],[xmax,ymax]]
+        # print(f"points : {bcid.rectangles[0].get_bbox().get_points()}")  #[[xmin,ymin],[xmax,ymax]]
 
         h = bcid.rectangles[0].get_height()
         bcid.rectangles[0].set_height(3*h)
@@ -182,5 +195,5 @@ class plt_one_addpt_onclick:
         ymax = bcid.rectangles[0].get_bbox().y1
         ymin = bcid.rectangles[0].get_bbox().y0
 
-        bcid.lines[0][0].set_ydata([ymax,ymin])
-        bcid.lines[0][1].set_ydata([ymin,ymax])
+        bcid.lines[0][0].set_ydata([ymax, ymin])
+        bcid.lines[0][1].set_ydata([ymin, ymax])
